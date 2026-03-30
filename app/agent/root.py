d02f39d67c5f35f9d55816fa67b7f77afea64ff0
hjkim <kimhj040105@gmail.com>
2026-03-30 10:15:29 +0900

    feat: 챗봇 다중 줄 입력 및 파일 첨부 기능 추가(종료 버그 수정)

--- app/agent/root.py
+++ app/agent/root.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.tools import AgentTool
 
-from app.agent.workflows import run_sequential_docu_summary_pipeline, run_parallel_tech_compare_pipeline, run_sequential_rag_pipeline
+from app.agent.workflows import run_sequential_docu_summary_pipeline, run_parallel_tech_compare_pipeline, run_sequential_rag_pipeline, run_sequential_docu_summary_pipeline
 from app.config.settings import settings
 from app.prompt.instructions import supervisor_instruction
 from app.tool.callbacks import before_agent_callback
@@ -16,12 +16,13 @@ rag_tool = AgentTool(run_sequential_rag_pipeline())
 supervisor_agent = LlmAgent(
     name="SupervisorAgent",
     model=settings.model,
-    instruction=supervisor_instruction,
-    tools=[
-        docu_summary_tool,
-        tech_compare_tool,
-        rag_tool
-    ],
+    instruction="당신은 친절한 답변 생성기입니다.",
+    # instruction=supervisor_instruction,
+    # tools=[
+    #     docu_summary_tool,
+    #     tech_compare_tool,
+    #     rag_tool
+    # ],
     before_agent_callback=before_agent_callback
 )
 
