955bc8da7088493ea8706107f17c81716653873c
joohyoung99 <joohyoung99@naver.com>
2026-03-30 14:54:29 +0900

    googlesearch추가,prompt수정

--- app/prompt/__init__.py
+++ app/prompt/__init__.py
@@ -1,22 +1,36 @@
 from app.prompt.instructions import (
-
-    merge_instruction,
-    rag_answer_instruction,
-    rag_rewrite_instruction,
-    rag_search_instruction,
-    save_to_file_instruction,
-    summary_only_instruction,
     supervisor_instruction,
+
+    rag_rewrite_instruction,
+    rag_answer_instruction,
+    rag_search_instruction,
+
+    parallel_rewrite_instruction,
+    parallel_web_search_instruction,
+    parallel_rag_search_instruction,
+    parallel_merge_instruction,
+    parallel_answer_instruction,
+
+    docu_rewrite_instruction,
+    docu_generation_instruction,
+
 )
 
 __all__ = [
+    "supervisor_instruction",
 
-    "merge_instruction",
-    "rag_answer_instruction",
     "rag_rewrite_instruction",
     "rag_search_instruction",
-    "save_to_file_instruction",
-    "summary_only_instruction",
-    "supervisor_instruction",
+    "rag_answer_instruction",
+    
+   
+    "parallel_rewrite_instruction",
+    "parallel_web_search_instruction",
+    "parallel_rag_search_instruction",
+    "parallel_merge_instruction",
+    "parallel_answer_instruction",
+    
+    "docu_rewrite_instruction",
+    "docu_generation_instruction",
 
 ]
